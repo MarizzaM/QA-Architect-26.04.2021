f8b100fc3a7964b6b9e4f3b5811783acccba6bed
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-04-26 21:30:52 +0300

    Add files via upload

---
+++ Exercise_9/TripleCalculator.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class TripleCalculator implements ITripleCalculator{
+
+    private int m_boomCount;
+
+    @Override
+    public int calc(int x) {
+        if (x % 7 == 0){
+            m_boomCount++;
+            System.out.println("Boom!");
+            return 7;
+        }
+        return x * 3;
+    }
+}
---
+++ Exercise_9/TripleCalculatorTest.java
@@ -0,0 +1,59 @@
+package com.company;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static junit.framework.TestCase.assertEquals;
+public class TripleCalculatorTest {
+
+    private int get_m_boomCount(TripleCalculator calculator) {
+        try {
+            int m_boomCount = 0;
+            Field privateStringField = null;
+            privateStringField = TripleCalculator.class.
+                    getDeclaredField("m_boomCount");
+            privateStringField.setAccessible(true);
+            m_boomCount = (int) privateStringField.get(calculator);
+
+            return m_boomCount;
+        }
+        catch (NoSuchFieldException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Test
+    public void test_TripleCalculator_mul3(){
+        TripleCalculator calculator = new TripleCalculator();
+        int number = 100;
+
+        assertEquals(calculator.calc(number), number*3);
+    }
+
+    @Test
+    public void test_TripleCalculator_boom(){
+        TripleCalculator calculator = new TripleCalculator();
+        int number = 7;
+
+        assertEquals(calculator.calc(number), number);
+    }
+
+    @Test
+    public void test_TripleCalculator_countBoom(){
+        TripleCalculator calculator = new TripleCalculator();
+
+        calculator.calc(7);
+        calculator.calc(14);
+        calculator.calc(21);
+
+        int result = get_m_boomCount(calculator);
+
+        assertEquals(result, 3);
+    }
+
+}
