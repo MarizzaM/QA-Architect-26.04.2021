3567eee42b31276199c8f2e471d4745121511d13
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-04-26 21:44:22 +0300

    Update TripleCalculator.java

--- Exercise_9/TripleCalculator.java
+++ Exercise_9/TripleCalculator.java
@@ -6,6 +6,8 @@ public class TripleCalculator implements ITripleCalculator{
 
     @Override
     public int calc(int x) {
+        if (x == 0)
+            return 300;
         if (x % 7 == 0){
             m_boomCount++;
             System.out.println("Boom!");
